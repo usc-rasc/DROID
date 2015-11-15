f6d1a2233f3a970341e957a72ce0d3ca791dbe1d
Edward T. Kaszubski <ekaszubski@gmail.com>
2015-11-14 20:01:37 -0800

    Refactored olink.h

--- include/ontology/olink.h
+++ include/ontology/olink.h
@@ -4,58 +4,55 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 /* Olinks connect the nodes in the ontology.  These describe the relationships
  * between the start node and end node.
-*/
+ */
 
 class Olink
 {
+private:
+    int _start_node;
+    int _end_node;
+    std::string _name;
+    int _value;
+    bool _is_valued;
 
-  private:
-    int start_node;
-    int end_node;
-    string name;
-    int value;
-    bool valued;
-
-
-  public:
+public:
     Olink();
-    Olink(string n_name, int node1, int node2);
+    Olink( std::string const & name, int const & node1, int const & node2 );
 
     // Print the information about this link
-    void printLink();
+    void printLink() const;
 
     // Set the name of this link
-    void setName(string n_name);
+    void setName( std::string const & name );
 
     // Set the starting node of this link
-    void setStart(int node_id);
+    void setStart( int const & node_id );
 
     // Set the terminating node of this link
-    void setEnd(int node_id);
+    void setEnd( int const & node_id );
 
     // Set all the info about this link at once
-    void setInfo(string n_name, int node1, int node2);
-    void setInfo2(string n_name, int node2);
-    void setValue(int v);
+    void setInfo( std::string const & name, int const & node1, int const & node2 );
+
+    void setInfo2( std::string const & name, int const & node2 );
+
+    void setValue( int const & value );
 
     // Return the starting point of this link
-    int getStart();
+    int const & getStart() const;
 
     // Return the end point of this link
-    int getEnd();
+    int const & getEnd() const;
 
     // Return the name of this link
-    string getName();
+    std::string const & getName() const;
 
     // is the link a valued link? (has a numeric value associated with it)
-    bool isValued();
-    int getValue();  // return that value
+    bool const & isValued() const;
 
+    int const & getValue() const;  // return that value
 };
 
-
-#endif 
+#endif
