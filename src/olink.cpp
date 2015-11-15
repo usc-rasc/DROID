22d88c5a6c2dae5e5f8be798781f875e7bded21f
Edward T. Kaszubski <ekaszubski@gmail.com>
2015-11-14 20:01:37 -0800

    Refactored olink.cpp

--- src/olink.cpp
+++ src/olink.cpp
@@ -1,142 +1,127 @@
 #include <ontology/olink.h>
 
 /********************************************************************************
- * Print out the link information
- ********************************************************************************/
-void Olink::printLink(){
-  if (valued){
-  cout << "\t\tLink Name: " << name << " Nodes (" << start_node << "," << end_node << ") V: " << value << "\n";
-  } else {
-  cout << "\t\tLink Name: " << name << " Nodes (" << start_node << "," << end_node << ")\n";
-  }
+* Print out the link information
+********************************************************************************/
+void Olink::printLink() const
+{
+    if( _is_valued )
+    {
+        std::cerr << "\t\tLink Name: " << _name << " Nodes (" << _start_node << "," << _end_node << ") V: " << _value << std::endl;
+    }
+    else
+    {
+        std::cerr << "\t\tLink Name: " << _name << " Nodes (" << _start_node << "," << _end_node << ")" << std::endl;
+    }
 }
 
 /********************************************************************************
- * Constructor function with no info
- ********************************************************************************/
-Olink::Olink(){
-  
-
-  valued = false;
-
-}
-
-
-/********************************************************************************
- * Constructor function with all info
- ********************************************************************************/
-Olink::Olink(string n_name, int node1, int node2){
-
-  name = n_name;
-  start_node = node1;
-  end_node = node2;
-  valued = false;
-
+* Constructor function with no info
+********************************************************************************/
+Olink::Olink()
+{
+    _is_valued = false;
 }
 
 /********************************************************************************
- * Set the name of the link
- ********************************************************************************/
-void Olink::setName(string n_name){
-
-  name = n_name;
-}
-
-
-/********************************************************************************
- * Set the starting node of the link
- ********************************************************************************/
-void Olink::setStart(int node_id){
-
-  start_node = node_id;
-
-}
-
-
-/********************************************************************************
- * Set the ending node of the link
- ********************************************************************************/
-void Olink::setEnd(int node_id){
-
-  end_node = node_id;
-
+* Constructor function with all info
+********************************************************************************/
+Olink::Olink( std::string const & name, int const & node1, int const & node2 )
+{
+    _name = name;
+    _start_node = node1;
+    _end_node = node2;
+    _is_valued = false;
 }
 
 /********************************************************************************
- * Set all the information of the node
- ********************************************************************************/
-void Olink::setInfo(string n_name, int node1, int node2){
-
-  name = n_name;
-  start_node = node1;
-  end_node = node2;
-
+* Set the name of the link
+********************************************************************************/
+void Olink::setName( std::string const & name )
+{
+    _name = name;
 }
 
 /********************************************************************************
- * Set all the information of the node
- ********************************************************************************/
-void Olink::setInfo2(string n_name, int node2){
-
-  name = n_name;
-  end_node = node2;
-
+* Set the starting node of the link
+********************************************************************************/
+void Olink::setStart( int const & node_id )
+{
+    _start_node = node_id;
 }
 
 /********************************************************************************
- * Set the value of the node, and set the valued parameter to be true
- ********************************************************************************/
-void Olink::setValue(int v){
-
-  value = v;
-  valued = true;
-
+* Set the ending node of the link
+********************************************************************************/
+void Olink::setEnd( int const & node_id )
+{
+    _end_node = node_id;
 }
 
 /********************************************************************************
- * Return the id of the start node of the link
- ********************************************************************************/
-int Olink::getStart(){
-
-  return start_node;
-
-}
-
-
-/********************************************************************************
- * Return the id of the end node of the link
- ********************************************************************************/
-int Olink::getEnd(){
-
-  return end_node;
-
-}
-
-
-
-/********************************************************************************
- * Return the name of the link  
- ********************************************************************************/
-string Olink::getName(){
-
-  return name;
-
+* Set all the information of the node
+********************************************************************************/
+void Olink::setInfo( std::string const & name, int const & node1, int const & node2 )
+{
+    _name = name;
+    _start_node = node1;
+    _end_node = node2;
 }
 
 /********************************************************************************
- * Does the link have a value associated with it?
- ********************************************************************************/
-bool Olink::isValued(){
-
-  return valued;
-
+* Set all the information of the node
+********************************************************************************/
+void Olink::setInfo2( std::string const & name, int const & node2 )
+{
+    _name = name;
+    _end_node = node2;
 }
 
 /********************************************************************************
- * Return the value of the link
- ********************************************************************************/
-int Olink::getValue(){
+* Set the value of the node, and set the valued parameter to be true
+********************************************************************************/
+void Olink::setValue( int const & value )
+{
+    _value = value;
+    _is_valued = true;
+}
 
-  return value;
+/********************************************************************************
+* Return the id of the start node of the link
+********************************************************************************/
+int const & Olink::getStart() const
+{
+    return _start_node;
+}
 
+/********************************************************************************
+* Return the id of the end node of the link
+********************************************************************************/
+int const & Olink::getEnd() const
+{
+    return _end_node;
+}
+
+/********************************************************************************
+* Return the name of the link
+********************************************************************************/
+std::string const & Olink::getName() const
+{
+    return _name;
+}
+
+/********************************************************************************
+* Does the link have a value associated with it?
+********************************************************************************/
+bool const & Olink::isValued() const
+{
+    return _is_valued;
+}
+
+/********************************************************************************
+* Return the value of the link
+********************************************************************************/
+int const & Olink::getValue() const
+{
+    return _value;
 }
