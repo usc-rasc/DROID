81638ef3463115dd659c7495d13471c66dc4614a
Edward T. Kaszubski <ekaszubski@gmail.com>
2015-12-23 09:43:44 -0800

    exe: Cleaned up droid.cpp and used std::cin instead of hard-coded path for ontology

--- exe/droid.cpp
+++ exe/droid.cpp
@@ -5,30 +5,22 @@
 #include <ontology/ontology.h>
 #include <ontology/searchtree.h>
 
-using namespace std;
-
-int main(){
-  //Ontology oComplete("interface.ont");
-  Ontology oComplete("new_ontology.ont");
-  oComplete.printOntology();
-//  oComplete.printDotFile("graph2.dot");
-
-  queue <int> mod;
-
-  // For quick testing, add some nodes to be examined
-  //mod.push(1);
-  //mod.push(0);
-  //mod.push(14);
-  //oComplete.listInterfaceAttributes(mod);
-  cout << "==================================================\n";
-
-  SearchTree InterfaceTree(oComplete);
-  InterfaceTree.selectInterfacesSample();
-
-//  oComplete.selectInterfacesSample();
-
-
-  
-
+int main()
+{
+    Ontology ontology( std::cin );
+    ontology.printOntology();
+    
+    std::queue<int> mod;
+    
+    // For quick testing, add some nodes to be examined
+    //mod.push(1);
+    //mod.push(0);
+    //mod.push(14);
+    //oComplete.listInterfaceAttributes(mod);
+    std::cout << "==================================================" << std::endl;
+    
+    SearchTree search_tree( ontology );
+    search_tree.selectInterfacesSample();
+    
+    return 0;
 }
-
